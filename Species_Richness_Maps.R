fd9de18b5244eb8ac3ab7c93acc85b2bee60d706
Mónica Gómez-Vadillo <127930485+mgomezv26@users.noreply.github.com>
2024-08-27 13:56:42 +0200

    Update Species_Richness_Maps.R

--- Species_Richness_Maps.R
+++ Species_Richness_Maps.R
@@ -1,21 +1,15 @@
+# The following script describe how to create the maps of the paper
 
-# Load library
-
+# Load library  ---------------------------------------------------------
 library(ggplot2) # version: 3.5.0 
 library(sf) # version: 1.0.16 
 library(viridis) # version: 0.6.5 
 library(patchwork) # version: 0.6.5  
 
-cat("ggplot2:", as.character(packageVersion("ggplot2")), "\n")
-cat("sf:", as.character(packageVersion("sf")), "\n")
-cat("viridis:", as.character(packageVersion("viridis")), "\n")
-cat("patchwork:", as.character(packageVersion("patchwork")), "\n")
-
-# Load data
+# Load data  ---------------------------------------------------------
 grid <- st_read("Data/shp/Grid_Richness.shp")
-colnames(grid)
-
-# ETRS89-LAEA coordinate system (EPSG:3035)
+#colnames(grid)
+# ETRS89-LAEA coordinate system (EPSG:3035) 
 grid <- st_transform(grid, crs = 3035)
 
 # Create a map for Odonata
@@ -35,7 +29,6 @@ map_lentic <- ggplot(data = grid) +
   theme(legend.position = "right") #hide axes with element_blank()
   #theme(legend.position = "right", axis.title.x = element_blank(), axis.title.y = element_blank()) #hide axes with element_blank()
 
-
 # Create a map for Lotic
 map_lotic <- ggplot(data = grid) +
   geom_sf(aes(fill = R_Lotic), color = NA) +
@@ -51,8 +44,21 @@ combined_map <- (map_od | map_lentic | map_lotic)
 # Show the combined figure
 print(combined_map)
 
+# Figure 1. Map of the study area. ---------------------------------------------------------
 
+labels <- c("Southern ", "Northern ")
+grid$Zone = as.factor(grid$Zone)
+map_zone <- ggplot(data = grid) +
+  geom_sf(aes(fill = Zone)) +
+  scale_fill_manual(values = c("#E0EEEE", "#49A4B9"), labels = labels) +
+  labs(title = "Study area", x = "Longitud", y = "Latitud") +
+  theme_minimal() +
+  theme(legend.position = "right")
 
+# Show
+map_zone
+
+# Supplementary Figure S2.2. ---------------------------------------------------------
 
 # Create a map for Anisoptera
 map_ani <- ggplot(data = grid) +
@@ -70,7 +76,6 @@ map_zyg <- ggplot(data = grid) +
   theme_minimal() +
   theme(legend.position = "right", axis.title.x = element_blank(), axis.title.y = element_blank())
 
-
 # Create a map for Anisoptera - Lentic
 map_ani_len <- ggplot(data = grid) +
   geom_sf(aes(fill = R_Ani_Len), color = NA) +
@@ -86,7 +91,6 @@ map_ani_lot <- ggplot(data = grid) +
   labs(title = "Anisoptera Lotic", x = "Longitud", y = "Latitud") +
   theme_minimal() +
   theme(legend.position = "right", axis.title.x = element_blank(), axis.title.y = element_blank()) #hide axes with element_blank()
-
 
 # Create a map for Zygoptera - Lentic
 map_zyg_len <- ggplot(data = grid) +
@@ -105,7 +109,6 @@ map_zyg_lot <- ggplot(data = grid) +
   theme(legend.position = "right", axis.title.x = element_blank(), axis.title.y = element_blank()) #hide axes with element_blank()
 
 # Combine the three maps into a single figure
-
 combined_map <- (map_ani | map_zyg) /
   (map_ani_len | map_ani_lot) /
   (map_zyg_len | map_zyg_lot)
@@ -113,16 +116,3 @@ combined_map <- (map_ani | map_zyg) /
 # Show the combined figure
 print(combined_map)
 
-# Create a map for Odonata
-labels <- c("Southern ", "Northern ")
-
-grid$Zone = as.factor(grid$Zone)
-
-map_zone <- ggplot(data = grid) +
-  geom_sf(aes(fill = Zone)) +
-  scale_fill_manual(values = c("#E0EEEE", "#49A4B9"), labels = labels) +
-  labs(title = "Study area", x = "Longitud", y = "Latitud") +
-  theme_minimal() +
-  theme(legend.position = "right")
-
-map_zone
